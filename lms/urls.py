c79cfd57b4b002fce9e0acfb4a3c7a1b06f5781d
Matthew Irizarry <matthewjirizarry@gmail.com>
2023-10-06 00:48:12 -0500

    Working discussion posts, and an interface in which to create them.

--- lms/urls.py
+++ lms/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('accounts/', include("django.contrib.auth.urls"))
+    path('accounts/', include("django.contrib.auth.urls")),
+    path("posts/create/", views.create_post, name="create_post")
 ]
--- lms/views.py
+++ lms/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import *
+from .forms import *
 
 # Create your views here.
 
@@ -20,3 +21,27 @@ def index(request):
     }
 
     return render(request, "pages/index.html", context)
+
+# ==========================
+# cslearning.com/posts/create
+# ==========================
+
+def create_post(request):
+    if request.method == "POST":
+        form = CreatePostForm(request.POST)
+
+        if (form.is_valid()):
+            author = request.user
+
+            created_post = form.save(commit=False)
+            created_post.author = author
+            created_post.save()
+
+            return redirect("index")
+
+    form = CreatePostForm()
+    context = {
+        "form": form
+    }
+
+    return render(request, "pages/posts/create_post.html", context)
